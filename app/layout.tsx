0f26b4b73613d5f41e59698dd21ece4b304f4a23
Shubham Kumar Bansal <shub@shubkb.com>
2024-11-16 00:37:11 +0530

    Add: Next Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function RootLayout({
   children,
@@ -9,6 +10,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Analytics />
+      <SpeedInsights />
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
